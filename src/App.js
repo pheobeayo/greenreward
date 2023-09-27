407a4694f77dc08efe55b0ec14291d26cf4660fb
pheobeayo <pheobeayo@gmail.com>
2023-09-27 12:01:30 +0100

    connect-button
    connect button modified

--- src/App.js
+++ src/App.js
@@ -7,14 +7,51 @@ import Dashboard from "./pages/dashboard/Dashboard";
 import Recyclingcentre from "./pages/reycling-centres/Recyclingcentre";
 import Badge from "./pages/badge/Badge";
 import VerificationBoard from "./pages/verification-board/VerificationBoard";
+import '@rainbow-me/rainbowkit/styles.css';
+import { darkTheme, getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { polygon } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [
+    // mainnet,
+    polygon,
+    //polygonMumbai,
+    // optimism,
+    // arbitrum,
+    // goerli,
+    // ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
+  ],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+});
+
+
+
 
 
 
 function App() {
   return (
-   
-
-
+    
+    <WagmiConfig config={wagmiConfig}>
+    <RainbowKitProvider chains={chains}
+    theme={darkTheme({
+      accentColor: 'rgb(139,195,74)'
+    })}>
 
 
         <BrowserRouter>
@@ -32,6 +69,9 @@ function App() {
 
           </Routes>
         </BrowserRouter>
+
+        </RainbowKitProvider>
+    </WagmiConfig> 
 
         
 
